ca3970ec2bfa425ca7f7b401db9a4738dccc4720
tunashred <enachealexandru.traian@gmail.com>
2024-03-10 17:36:26 +0200

    Documented header files

--- hashtable-fancy/include/hashtable.h
+++ hashtable-fancy/include/hashtable.h
@@ -32,6 +32,18 @@ typedef struct hashtable {
     struct bucket* buckets;
 } hashtable;
 
+/**
+ * @brief Hashtable creation
+ * 
+ * @param bucketCount       number of buckets
+ * @param p_func_hashCode   function pointer to hash function
+ * @param p_func_equals     function pointer to comparison function
+ * @param p_func_printKey   function pointer to printKey function
+ * @param p_func_printValue function pointer to printValue function
+ * @param p_func_freeKey    function pointer to free key function
+ * @param p_func_freeValue  function pointer to free value function
+ * @return hashtable* instance ready to use
+ */
 hashtable* hashtable_create(
         unsigned int bucketCount,
         unsigned long (*p_func_hashCode)(const void*),
@@ -42,10 +54,52 @@ hashtable* hashtable_create(
         void (*p_func_freeValue)(void*)
 );
 
+/**
+ * @brief Hashtable add entry
+ * 
+ * @param ht      instance of hashtable
+ * @param key     key to be hashed
+ * @param value   storing value
+ * @returns
+ *      - true:  if the entry was added successfully
+ *      - false: if the hashtable or key is NULL
+ */
 bool hashtable_add(hashtable* ht, const void* key, const void* value);
+
+/**
+ * @brief Get value by key from hashtable
+ * 
+ * @param ht  instance of hashtable
+ * @param key search key
+ * @returns
+ *      - void* value: if entry found
+ *      - null: 
+ *           - if hashtable or key is NULL
+ *           - if entry not found
+ */
 void* hashtable_get(hashtable* ht, const void* key);
+
 void hashtable_print(hashtable* ht);
+
+/**
+ * @brief Removes entry matching key
+ * 
+ * @param ht  instance of hashtable
+ * @param key search key
+ * @returns
+ *      - void* value: if entry was removed
+ *      - null: 
+ *           - if hashtable or key is NULL
+ *           - if hashtable is empty
+ *           - if entry not found
+ */
 void* hashtable_remove(hashtable* ht, const void* key);
+
+/**
+ * @brief    Frees instance of hashtable and all its entries
+ * 
+ * @param ht instance of hashtable
+ */
 void hashtable_destroy(hashtable* ht);
 
 #endif
--- hashtable-fancy/include/hashtable_type_util_charp.h
+++ hashtable-fancy/include/hashtable_type_util_charp.h
@@ -4,9 +4,33 @@
 #include <string.h>
 #include <stdbool.h>
 
+/**
+ * @brief Hashes void* key as char*
+ * 
+ * @param key 
+ * @return unsigned long hash
+ */
 unsigned long hash_void_charp(const void* key);
+
+/**
+ * @brief Compares two void* variables as char*
+ * 
+ * @param str1 
+ * @param str2 
+ * @returns
+ *      - true: if str1 is equal with str2
+ *      - false: otherwise
+ */
 bool equals_void_charp(const void* str1, const void* str2);
+
+/**
+ * @brief Returns a char* copy of void* str
+ * 
+ * @param str 
+ * @return char* 
+ */
 char* print_void_charp(const void* str);
+
 void free_void_charp(void* str);
 
 #endif
